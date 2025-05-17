9730ad5ae6a90fb37306355d90c2993b5156932f
Tyler <tyler2.jarboe@gmail.com>
2025-05-17 11:54:06 -0400

    Small modifications seperated the cmp

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,7 +1,3 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-if true then return {} end
-
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
@@ -9,6 +5,7 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  --[[
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
@@ -19,6 +16,7 @@ return {
       colorscheme = "gruvbox",
     },
   },
+  --]]
 
   -- change trouble config
   {
@@ -29,16 +27,6 @@ return {
 
   -- disable trouble
   { "folke/trouble.nvim", enabled = false },
-
-  -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
 
   -- change some telescope options and a keymap to browse plugin files
   {
@@ -176,8 +164,8 @@ return {
     end,
   },
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  -- -- use mini.starter instead of alpha
+  -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
@@ -191,6 +179,10 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "eslint-lsp",
+        "js-debug-adapter",
+        "prettier",
+        "typescript-language-server",
       },
     },
   },
