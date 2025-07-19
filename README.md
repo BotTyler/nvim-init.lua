7032b19955f42feacd8e4255c8f26a58e8bed3d0
Tyler <tyler2.jarboe@gmail.com>
2025-07-19 11:48:19 -0400

    Refactor: Modified README to incldue some required packages. Bug: Fixed Conform config to not throw an error

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 ```bash
 git clone https://github.com/BotTyler/nvim-init.lua.git ~/.config/nvim
 
+# Deps
+  - Git (base-devel)
+  - Nerd Font (Icons).
+  - Lua Rocks (Optional)
+
 ```
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,24 @@
-require("conform").setup({
-    formatters_by_ft = {
-        html = { "prettier" },
-        htmlangular = { "prettier" },
+return {
+    {
+        "stevearc/conform.nvim",
+        opts = function(_, opts)
+            -- Extend formatters_by_ft to add prettier
+            opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                html = { "prettier" },
+                htmlangular = { "prettier" },
+                css = { "prettier" },
+                json = { "prettier" },
+                markdown = { "prettier" },
+                -- add any other filetypes you want here
+            })
+
+            -- Configure the prettier formatter
+            opts.formatters = opts.formatters or {}
+            opts.formatters.prettier = {
+                prepend_args = { "--print-width", "50" },
+            }
+        end,
     },
-    formatters = {
-        prettier = {
-            prepend_args = { "--print-width", "100" },
-        },
-    },
-})
+}
