b3200a19378dabf5d5f80b581ada9b37a8794206
Tyler <tyler2.jarboe@gmail.com>
2026-01-21 22:18:01 -0500

    Fixed bug with snacks buffer. Removed unused keymaps.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,11 +18,14 @@ return {
         dependencies = {
             "jose-elias-alvarez/typescript.nvim",
             init = function()
-                require("snacks").util.lsp.on(function(_, buffer)
-                -- require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-                    vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+                require("snacks").util.lsp.on(function(bufnr)
+                    vim.keymap.set(
+                        "n",
+                        "<leader>co",
+                        "TypescriptOrganizeImports",
+                        { buffer = bufnr, desc = "Organize Imports" }
+                    )
+                    vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = bufnr })
                 end)
             end,
         },
